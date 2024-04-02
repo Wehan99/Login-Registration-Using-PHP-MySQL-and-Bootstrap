5af830cd78c7a47f403bbaa9b1da805f1a8f1d2f
Wehan99 <wehanhimsara@gmail.com>
2024-04-02 13:04:03 +0530

    new commit

---
+++ signup/landing.php
@@ -0,0 +1,40 @@
+<?php
+
+?>
+
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Login Page</title>
+  </head>
+  <body>
+
+
+
+
+  
+    <div class="container mt-5">
+    <h1 class = "text-center">Welcome to PHP login Signup</h1>
+    <div class="mb-3">
+    
+    </div>
+   
+    <div class="text-center"> 
+        <a href="login.php" class="btn btn-primary btn-lg mx-2">Login</a> 
+        <a href="sign.php" class="btn btn-primary btn-lg mx-2">Signup</a> 
+    </div>
+
+    </div>
+
+
+  </body>
+</html>
